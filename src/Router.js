bb6f73cd4e99882d734ae9cb0e96b06396d57d75
HSH-97 <shh6297@gmail.com>
2024-07-01 17:47:38 +0900

    router setup

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+const Router = () => {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={""} />
+        <Route path="/login" element={""} />
+        <Route path="/signup" element={""} />
+        <Route path="/" element={""} />
+      </Routes>
+    </HashRouter>
+  );
+};
