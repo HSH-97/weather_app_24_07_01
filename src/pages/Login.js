d004dcf74239507106554687aa1e74d9751c36e2
HSH-97 <shh6297@gmail.com>
2024-07-05 17:08:04 +0900

    deploy

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import { Title } from "../components/Title";
+
+export const Login = () => {
+  return (
+    <div>
+      <Title titleName={"LOGIN"} />
+    </div>
+  );
+};
---
+++ src/pages/Signup.js
@@ -0,0 +1,9 @@
+import { Title } from "../components/Title";
+
+export const Signup = () => {
+  return (
+    <div>
+      <Title titleName={"SIGN UP"} />
+    </div>
+  );
+};
